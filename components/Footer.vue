cd6b0a59692590bc35cecb90cd07c84af29e9f35
Zzerium <87914117+Zzerium@users.noreply.github.com>
2023-10-17 19:25:35 -0400

    chore(footer): add git repo link

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,14 +3,14 @@
     <footer
       class="flex flex-wrap justify-center items-center text-black dark:text-white text-sm gap-2 p-2"
     >
-      <!--<a
+      <a
         href="https://github.com/johnbotapp/whois"
         target="_blank"
         class="hover:underline"
       >
-        source code
+        Source
       </a>
-      <div class="h-1 w-1 bg-black dark:bg-white rounded-full"></div>-->
+      <div class="h-1 w-1 bg-black dark:bg-white rounded-full"></div>
       <span>
         Made with <a 
           href="https://en.wikipedia.org/wiki/Heart"
